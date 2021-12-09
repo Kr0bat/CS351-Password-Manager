c42fc71326e36ec9b04f360f9bcb30f68838ccaf
yarmout <58712447+yarmout@users.noreply.github.com>
2021-12-09 18:02:18 -0500

    Add account_exists function

--- functions.py
+++ functions.py
@@ -1,3 +1,11 @@
-def exists(json, domain, username):
-    if json[domain][username]:
-        print('Success!')
+import json
+
+
+def account_exists(json_file_name, domain, username):
+    with open(json_file_name) as json_file:
+        data = json.load(json_file)
+        try:
+            if data[domain][username]:
+                return True
+        except KeyError:
+            return False
