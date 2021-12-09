2cfe25bee1f9af7e723d9f7e637ed0a0ef7a1386
yarmout <58712447+yarmout@users.noreply.github.com>
2021-12-09 18:18:47 -0500

    Add retrieve_account function which retrieves the password of the requested account

--- functions.py
+++ functions.py
@@ -1,11 +1,32 @@
 import json
 
+jsonFileName = 'example.json'
 
-def account_exists(json_file_name, domain, username):
+
+# Checks if account exists
+# Returns the json file as a dict if it exists
+# Else returns None
+def __account_exists(json_file_name, domain, username):
     with open(json_file_name) as json_file:
         data = json.load(json_file)
         try:
             if data[domain][username]:
-                return True
+                return data
         except KeyError:
-            return False
+            return None
+
+
+# Retrieve linked account's password
+# Return None if it does not exist
+def retrieve_account(domain, username):
+    return __retrieve_account_helper(jsonFileName, domain, username)
+
+
+def __retrieve_account_helper(json_file_name, domain, username):
+    data = __account_exists(json_file_name, domain, username)
+    if not data:
+        return None
+    return data[domain][username]
+
+
+print(retrieve_account('reddit.com', 'M'))
